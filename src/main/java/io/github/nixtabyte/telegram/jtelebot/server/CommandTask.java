cfb19cdd3f3e3202bfc8047c2945f6376c9ec3cb
rdestrada <dominguezestrada@gmail.com>
2015-07-10 00:36:17 -0500

    Added javadoc comments.

--- src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandTask.java
+++ src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandTask.java
@@ -8,8 +8,20 @@
  */
 package io.github.nixtabyte.telegram.jtelebot.server;
 
+/**
+ * This interface represents an isolated task action that shall be able to
+ * handle the execution of an individual {@link Command} object. Its
+ * implementation could be done by means of a single Thread that could be part
+ * of a ThreadPool or ThreadExecutor.
+ * 
+ * @see Command
+ * @since 0.0.1
+ * */
 public interface CommandTask {
 
+	/**
+	 * Method used to perform the individual {@link Command} execution.
+	 * */
 	public void processCommand();
 
 }
