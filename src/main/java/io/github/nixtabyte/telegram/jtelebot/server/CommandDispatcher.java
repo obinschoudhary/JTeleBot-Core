91306a3ed52331897d6eae31bab61c1a77df1ef4
rdestrada <dominguezestrada@gmail.com>
2015-07-10 00:28:14 -0500

    Added javadoc comments.

--- src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandDispatcher.java
+++ src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandDispatcher.java
@@ -8,9 +8,29 @@
  */
 package io.github.nixtabyte.telegram.jtelebot.server;
 
+/**
+ * This interface represents a listener component that shall be able to execute
+ * or dispatch {@link Command} instances stored in an internal queue. These
+ * commands are the instructions given by the Telegram User to the corresponding
+ * Bot implementation.
+ * 
+ * @see Command
+ * @since 0.0.1
+ * */
 public interface CommandDispatcher {
 
+	/**
+	 * Method to allow adding a {@link Command} object to the internal queue.
+	 * 
+	 * @param command
+	 *            Is the {@link Command} instance to be enqueued.
+	 * 
+	 * */
 	public void enqueueCommand(Command command);
 
+	/**
+	 * Method used to indicate this CommandDispatcher to execute every
+	 * {@link Command} stored in the internal queue.
+	 * */
 	public void dispatchCommands();
 }
--- src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandWatcher.java
+++ src/main/java/io/github/nixtabyte/telegram/jtelebot/server/CommandWatcher.java
@@ -8,8 +8,24 @@
  */
 package io.github.nixtabyte.telegram.jtelebot.server;
 
+import io.github.nixtabyte.telegram.jtelebot.response.json.Message;
+import io.github.nixtabyte.telegram.jtelebot.response.json.Update;
+
+/**
+ * This interface represents a listener component that sends polling requests to
+ * the Telegram Bot API service in order to fetch new {@link Updates} for the
+ * corresponding Bot. Every {@link Update} object contains a {@link Message}
+ * instance that is encapsulated into a {@link Command} object. </br></br> For
+ * this reason the CommandWatcher is responsible for retrieving all available
+ * Telegram User commands to manipulate the corresponding Bot.
+ * 
+ * */
 public interface CommandWatcher {
 
+	/**
+	 * This method is used to perform the Telegram polling service in order to
+	 * retrieve all available commands for the Bot manipulation.
+	 * */
 	public void retrieveCommands();
 
 }
