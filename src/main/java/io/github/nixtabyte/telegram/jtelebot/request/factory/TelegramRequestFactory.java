b1b86fe555f8a82f7a860716210e970870efb578
Juan Carlos Sedano Salas <jc.sedano.isc@gmail.com>
2015-07-09 23:03:48 -0500

    UPDATED: TelegramRequestFactory with documentation

--- src/main/java/io/github/nixtabyte/telegram/jtelebot/request/factory/TelegramRequestFactory.java
+++ src/main/java/io/github/nixtabyte/telegram/jtelebot/request/factory/TelegramRequestFactory.java
@@ -26,6 +26,10 @@ import org.codehaus.jackson.map.JsonMappingException;
 /**
  * 
  * This class is a Factory of TelegramRequest objects
+ * it allows to easily create a request, for more information
+ * on the methods that the Telegram Bots api support please check
+ * https://core.telegram.org/bots/api#available-methods
+ * 
  *
  */
 public final class TelegramRequestFactory {
@@ -37,20 +41,26 @@ public final class TelegramRequestFactory {
 	
 	private TelegramRequestFactory(){}
 	/**
+	 * Creates a request to use the getMe method of the Telegram Bot api
 	 * 
-	 * @return
+	 * @return A TelegramRequest prepared to consume the getMe method
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createGetMeRequest(){
 		return new TelegramRequest(RequestType.GET_ME,new ArrayList<BasicNameValuePair>());
 	}
+	
 	/**
 	 * 
-	 * @param chatId
-	 * @param text
-	 * @param disableWebPagePreview
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a Telegram request ready to use to consume the sendMessage method of the Telegram bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param text Text of the message to be sent
+	 * @param disableWebPagePreview Optional - Disables link previews for links in this message
+	 * @param replyToMessageId	Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A TelegramRequest prepared to consume the sendMessage method
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -66,10 +76,13 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param fromChatId
-	 * @param messageId
-	 * @return
+	 * Creates a request to use the getMe method of the Telegram Bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param fromChatId Unique identifier for the chat where the original message was sent — User or GroupChat id
+	 * @param messageId Unique message identifier
+	 * @return A request to use the getMe method of the Telegram Bot api
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createForwardMessageRequest(final int chatId,final int fromChatId,final int messageId){
 		List <BasicNameValuePair>basicNameValuePair = new ArrayList<BasicNameValuePair>();
@@ -80,12 +93,15 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param inputFile
-	 * @param caption
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a request to use the sendPhoto method of the Telegram Bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param inputFile Photo to upload 
+	 * @param caption Optional - Photo caption (may also be used when resending photos by file_id).
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A request to use the sendPhoto method of the Telegram Bot api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -98,14 +114,18 @@ public final class TelegramRequestFactory {
 		TelegramRequestFactory.addIfNotNull("reply_markup",customReplyKeyboard,basicNameValuePair);
 		return new TelegramRequest(RequestType.SEND_PHOTO,basicNameValuePair,inputFile,"photo");
 	}
+
 	/**
 	 * 
-	 * @param chatId
-	 * @param photoId
-	 * @param caption
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a request to use the sendPhoto method of the Telegram Bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param photoId Re-send a photo that is already on the Telegram servers
+	 * @param caption Optional - Photo caption (may also be used when resending photos by file_id).
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A request to use the sendPhoto method of the Telegram Bot api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -121,11 +141,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param inputFile
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a request to use the sendAudio method of the Telegram Bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param inputFile Audio file to send 
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A request to use the sendAudio method of the Telegram Bot api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -139,11 +162,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param audioId
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a request to use the sendAudio method of the Telegram Bot api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param audioId Re-send an audio that is already on the Telegram servers
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A request to use the sendAudio method of the Telegram Bot api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -156,13 +182,16 @@ public final class TelegramRequestFactory {
 		TelegramRequestFactory.addIfNotNull("reply_markup",customReplyKeyboard,basicNameValuePair);
 		return new TelegramRequest(RequestType.SEND_AUDIO,basicNameValuePair);
 	}
+	
 	/**
+	 * Creates a telegram request ready to consume the sendDocument request method of the Telegram Bot Api
 	 * 
-	 * @param chatId
-	 * @param inputFile
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param inputFile File to send
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendDocument request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -174,13 +203,16 @@ public final class TelegramRequestFactory {
 		TelegramRequestFactory.addIfNotNull("reply_markup",customReplyKeyboard,basicNameValuePair);
 		return new TelegramRequest(RequestType.SEND_DOCUMENT,basicNameValuePair,inputFile,"document");
 	}
+	
 	/**
+	 * Creates a telegram request ready to consume the sendDocument request method of the Telegram Bot Api
 	 * 
-	 * @param chatId
-	 * @param documentId
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param documentId Re-send a file that is already on the Telegram servers, or upload a new file using multipart/form-data.
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendDocument request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -195,11 +227,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param inputFile
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a telegram request ready to consume the sendSticker request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param inputFile Sticker to upload
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendSticker request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -213,11 +248,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param stickerId
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a telegram request ready to consume the sendSticker request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param stickerId Re-send a sticker that is already on the Telegram servers
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendSticker request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -232,11 +270,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param inputFile
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a telegram request ready to consume the sendVideo request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param inputFile Video to upload
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendVideo request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -250,11 +291,14 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param videoId
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a telegram request ready to consume the sendVideo request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param videoId Re-send a video that is already on the Telegram servers.
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendVideo request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -270,12 +314,15 @@ public final class TelegramRequestFactory {
 	
 	/**
 	 * 
-	 * @param chatId
-	 * @param latitude
-	 * @param longitude
-	 * @param replyToMessageId
-	 * @param customReplyKeyboard
-	 * @return
+	 * Creates a telegram request ready to consume the sendLocation request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param latitude Latitude of location
+	 * @param longitude Longitude of location 
+	 * @param replyToMessageId Optional - If the message is a reply, ID of the original message
+	 * @param customReplyKeyboard Optional - Additional interface options. A JSON-serialized object for a custom reply keyboard, instructions to hide keyboard or to force a reply from the user.
+	 * @return A telegram request ready to consume the sendLocation request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 * @throws JsonGenerationException
 	 * @throws JsonMappingException
 	 * @throws IOException
@@ -291,9 +338,12 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param chatId
-	 * @param broadcastActionType
-	 * @return
+	 * Creates a telegram request ready to consume the sendChatAction request method of the Telegram Bot Api
+	 * 
+	 * @param chatId Unique identifier for the message recipient — User or GroupChat id
+	 * @param broadcastActionType Type of action to broadcast. Choose one, depending on what the user is about to receive: typing for text messages, upload_photo for photos, record_video or upload_video for videos, record_audio or upload_audio for audio files, upload_document for general files, find_location for location data.
+	 * @return A telegram request ready to consume the sendChatAction request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createSendChatActionRequest(final int chatId,final BroadcastActionType broadcastActionType){
 		List <BasicNameValuePair>basicNameValuePair = new ArrayList<BasicNameValuePair>();
@@ -303,10 +353,13 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param userId
-	 * @param offset
-	 * @param limit
-	 * @return
+	 * Creates a telegram request ready to consume the getUserProfilePhotos request method of the Telegram Bot Api
+	 * 
+	 * @param userId Unique identifier of the target user
+	 * @param offset Sequential number of the first photo to be returned. By default, all photos are returned.
+	 * @param limit Limits the number of photos to be retrieved. Values between 1—100 are accepted. Defaults to 100.
+	 * @return A telegram request ready to consume the getUserProfilePhotos request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createGetUserProfilePhotosRequest(final int userId,final Integer offset, final Integer limit){
 		List <BasicNameValuePair>basicNameValuePair = new ArrayList<BasicNameValuePair>();
@@ -317,10 +370,13 @@ public final class TelegramRequestFactory {
 	}
 	/**
 	 * 
-	 * @param offset
-	 * @param limit
-	 * @param timeout
-	 * @return
+	 * Creates a telegram request ready to consume the getUpdates request method of the Telegram Bot Api
+	 * 
+	 * @param offset Optional - Identifier of the first update to be returned. Must be greater by one than the highest among the identifiers of previously received updates. By default, updates starting with the earliest unconfirmed update are returned. An update is considered confirmed as soon as getUpdates is called with an offset higher than its update_id.
+	 * @param limit Optional - Limits the number of updates to be retrieved. Values between 1—100 are accepted. Defaults to 100
+	 * @param timeout Optional - Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling
+	 * @return A telegram request ready to consume the getUpdates request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createGetUpdatesRequest(final Long offset, final Long limit, final Long timeout){
 		List <BasicNameValuePair>basicNameValuePair = new ArrayList<BasicNameValuePair>();
@@ -332,8 +388,11 @@ public final class TelegramRequestFactory {
 	
 	/**
 	 * 
-	 * @param url
-	 * @return
+	 * Creates a telegram request ready to consume the setWebhook request method of the Telegram Bot Api
+	 * 
+	 * @param url HTTPS url to send updates to. Use an empty string to remove webhook integration
+	 * @return A telegram request ready to consume the setWebhook request method of the Telegram Bot Api
+	 * @see TelegramRequest
 	 */
 	public static TelegramRequest createSetWebhookRequest(final String url){
 		List <BasicNameValuePair>basicNameValuePair = new ArrayList<BasicNameValuePair>();
